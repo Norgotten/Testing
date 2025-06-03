b7b632bac1b2eab8dd9ffae4a83b918ad8f6a31d
Hafiz Ramadhan <hafiz.ramadhan06@gmail.com>
2025-06-03 19:52:18 +0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -2,19 +2,57 @@
 #include "kernel.h"
 #include "std_lib.h"
 
-void shell(){
+void shell() {
   char buf[128];
-  printString("Welcome to EorzeOS!\n");
+  char username[32];
+  
+  // Inisialisasi username
+  strcpy(username, "user");
+
+  // Sambut user
+  printString("Welcome to EorzeOS!\r\n");
+
   while (true) {
+    // Bersihkan buffer input sebelum dipakai lagi
+    clear((byte*)buf, 128);
+
+    // Tampilkan prompt
+    printString(username);
     printString("> ");
+
+    // Baca input (readString() akan mencetak "\r\n" saat ENTER)
     readString(buf);
-    printString(buf);
-    printString("\n");
+
+    // Proses perintah:
+    if (strcmp(buf, "yo") == 1) {
+      // Hanya mencetak sekali "\r\n", bukan "\n\n"
+      printString("gurt\r\n");
+    }
+    else if (strcmp(buf, "gurt") == 1) {
+      printString("yo\r\n");
+    }
+    else if (strncmp(buf, "user ", 5) == 1) {
+      strcpy(username, buf + 5);
+      printString("Username changed to ");
+      printString(username);
+      printString("\r\n");
+    }
+    else if (strcmp(buf, "user") == 1) {
+      strcpy(username, "user");
+      printString("Username changed to user\r\n");
+    }
+    else {
+      // Kalau bukan command khusus, echo kembali:
+      printString(buf);
+      printString("\r\n");
+    }
+
+    // TIDAK perlu printString("\r\n") lagi di sini,
+    // karena setiap blok 'printString(..."\r\n")' sudah
+    // mengembalikan kursor ke kolom 0 pada baris baru.
   }
 }
 
 void parseCommand(char *buf, char *cmd, char arg[2][64]) {
-  //TODO: Implementasi fungsi untuk mem-parsing perintah
+  // Belum terpakai sekarang
 }
-
-// Tambahkan fungsi bila perlu
